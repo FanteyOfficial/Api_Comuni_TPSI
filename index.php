59009dfb765d65cee4c51c44277233336232e773
Lorenzo D'Aniello <lorida1602@gmail.com>
2024-04-13 09:16:05 +0200

    update

--- index.php
+++ index.php
@@ -28,6 +28,37 @@
                 background-color: #f0f0f0;
                 border-radius: 3px;
             }
+
+            /* Style for the form */
+            form {
+                display: flex;
+                flex-direction: column;
+                align-items: start;
+                gap: 10px;
+            }
+
+            /* Style for the input fields */
+            input[type="text"] {
+                padding: 5px;
+                border: 1px solid #ccc;
+                border-radius: 3px;
+                width: 200px;
+            }
+
+            /* Style for the submit buttons */
+            input[type="submit"] {
+                padding: 5px 10px;
+                border: none;
+                border-radius: 3px;
+                background-color: #007BFF;
+                color: white;
+                cursor: pointer;
+            }
+
+            /* Change the background color of the submit buttons on hover */
+            input[type="submit"]:hover {
+                background-color: #0056b3;
+            }
         </style>
     </head>
     <body>
@@ -38,11 +69,15 @@
             <input type="submit" value="elimina" onclick="deleteComune();" />
         </form>
 
+        <hr>
+
         <form action="" method="post">
             <input type="text" name="comune" id="modComune" placeholder="Nuovo nome" />
             
             <input type="submit" value="Modifica" onclick="updateComune();" />
         </form>
+
+        <hr>
 
         <div class=".cont-data">
             <ul id="cont-data"></ul>
